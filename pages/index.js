7633496b7bfc950bfb4f3ebf84ffc992b815ec34
Trevor Williams <tmw7991@gmail.com>
2023-05-16 10:28:05 -0600

    added favicon and title

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
+import Head from "next/head";
 import Calendar from "@/components/Calendar";
 import TaskProvider from "@/contexts/TaskContext";
 
 function Home() {
   return (
-    <TaskProvider>
-      <Calendar />
-    </TaskProvider>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/logo.png" />
+        <title>Calendar</title>
+      </Head>
+      <TaskProvider>
+        <Calendar />
+      </TaskProvider>
+    </>
   );
 }
 
