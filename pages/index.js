788615333ac97ec59a82e64fb42ab22c1600712a
Trevor Williams <tmw7991@gmail.com>
2023-05-16 11:12:06 -0600

    added document file

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,11 @@
-import Head from "next/head";
 import Calendar from "@/components/Calendar";
 import TaskProvider from "@/contexts/TaskContext";
 
 function Home() {
   return (
-    <>
-      <Head>
-        <link rel="shortcut icon" href="/logo.png" />
-        <title>Calendar</title>
-      </Head>
-      <TaskProvider>
-        <Calendar />
-      </TaskProvider>
-    </>
+    <TaskProvider>
+      <Calendar />
+    </TaskProvider>
   );
 }
 
